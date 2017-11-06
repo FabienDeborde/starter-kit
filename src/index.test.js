96fd691068b74b2d93fda828ffb9f631f60b8c63
Fabien <fabien.webdev@gmail.com>
2017-11-06 15:00:02 +0900

    Add Web server, automation, transpiling, bundling, linting, testing & CI

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import fs from 'fs';
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true)
+  });
+});
+
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.querySelector("h1").textContent;
+    done();
+    expect(h1).to.equal('Hello');
+  })
+})
